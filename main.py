27c92798954ca81c9ae25497c257edc48db55003
numeezbaloch17 <numeezbaloch17@gmail.com>
2024-06-04 14:48:25 +0530

    Added code to draw lines on the canvas

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Window:
        self.width = width
        self.root = Tk()
        self.root.title("Maze Solver")
+       self.root.geometry(f"{self.height}x{self.width}")
        self.canvas = Canvas()
        self.canvas.pack()
        self.is_running = False
@@ -23,6 +24,23 @@ class Window:
     def close(self):
         self.is_running=False
 
+    def draw_line(self,line,fill_color:str):
+        line.draw(self.canvas,fill_color)
+
+
+class Point:
+    def __init__(self,x,y) -> None:
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self,point1,point2) -> None:
+        self.point1 = point1
+        self.point2 = point2
+    
+    def draw(self,canvas,fill_color):
+        canvas.create_line(self.point1.x,self.point1.y,self.point2.x,self.point2.y,fill = fill_color,width=2)
+
 
 
         
@@ -36,7 +54,9 @@ class Window:
     
 
 def main():
-    win = Window(800, 600)
+   
+    win = Window(1000, 1000)
+
     win.wait_for_close()
 
 
